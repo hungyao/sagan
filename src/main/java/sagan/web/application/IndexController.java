29ab482263dfa4598a0654dee0a1d79b80f05f98
Andrew Tjia <hungyao@tjia.ca>
2012-04-21 19:57:21 -0700

    added google globe html5 experiment

---
+++ src/main/java/sagan/web/application/IndexController.java
@@ -0,0 +1,14 @@
+package sagan.web.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+}
